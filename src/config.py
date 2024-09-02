59ecb22fd3d3af0b9d25f5cbcd1a46d8e79cea57
john@2simple.com <john@2simple.com>
2024-09-02 19:21:29 +0100

    add pwd

--- src/config.py
+++ src/config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+POSTGRES_PWD = config['POSTGRES_PWD']
 
 SECRET_KEY = os.urandom(32)
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:p0stgr3sM1l0!@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:' + POSTGRES_PWD + '@localhost:5432/fyyur'
