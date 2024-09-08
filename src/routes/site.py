cc3af9069b3fb159e45df995c597125626c69891
john@2simple.com <john@2simple.com>
2024-09-08 11:16:50 +0100

    add @app.before_request

--- src/routes/site.py
+++ src/routes/site.py
@@ -2,18 +2,23 @@
 Site controller
 """
 from src.populate import populate_db
-from flask import render_template
+from flask import render_template, request
 
 dummy_data_inserted = False
 
 def setup(app):
-    @app.route('/')
-    def index():
+
+    @app.before_request
+    def before_every_request():
         global dummy_data_inserted
         if not dummy_data_inserted:
             populate_db()
             dummy_data_inserted = True
 
+
+
+    @app.route('/')
+    def index():
         return render_template('pages/home.html')
     
 
